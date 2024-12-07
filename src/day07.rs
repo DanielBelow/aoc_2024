bc811578fdf0b789c0d0c3b95eeaa009e3b0feb4
DanielBelow <daniel.below@tum.de>
2024-12-07 07:16:33 +0100

    + day07

---
+++ src/day07.rs
@@ -0,0 +1,129 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
+
+#[derive(Clone, Debug)]
+pub struct Equation {
+    target: i64,
+    numbers: Vec<i64>,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Operator {
+    Plus,
+    Mul,
+    Concat,
+}
+
+impl Equation {
+    fn can_solve(&self, ops: &[Operator], available_ops: &[Operator]) -> bool {
+        let req_ops = self.numbers.len() - 1;
+        if ops.len() != req_ops {
+            for &op in available_ops {
+                let mut new_ops = ops.to_vec();
+                new_ops.push(op);
+                if self.can_solve(&new_ops, available_ops) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        let mut res = self.numbers[0];
+        for idx in 1..self.numbers.len() {
+            if res > self.target {
+                return false;
+            }
+
+            let op = ops[idx - 1];
+            let n = self.numbers[idx];
+            match op {
+                Operator::Plus => res += n,
+                Operator::Mul => res *= n,
+                Operator::Concat => res = format!("{res}{n}").parse::<i64>().expect("concat"),
+            };
+        }
+
+        res == self.target
+    }
+}
+
+#[aoc_generator(day07)]
+pub fn generate(s: &str) -> Vec<Equation> {
+    s.lines()
+        .map(|l| {
+            let mut spl = l.split(": ");
+
+            let target = spl
+                .next()
+                .and_then(|n| n.parse::<i64>().ok())
+                .expect("lhs: rhs1...");
+            let numbers = spl
+                .next()
+                .and_then(|n| {
+                    n.split_ascii_whitespace()
+                        .filter_map(|n| n.parse::<i64>().ok())
+                        .collect_vec()
+                        .into()
+                })
+                .expect("lhs: rhs1...");
+
+            Equation { target, numbers }
+        })
+        .collect_vec()
+}
+
+#[aoc(day07, part1)]
+pub fn part1(inp: &[Equation]) -> i64 {
+    let mut result = 0;
+
+    for eq in inp {
+        if eq.can_solve(&[], &[Operator::Plus, Operator::Mul]) {
+            result += eq.target;
+        }
+    }
+
+    result
+}
+
+#[aoc(day07, part2)]
+pub fn part2(inp: &[Equation]) -> i64 {
+    let mut result = 0;
+
+    for eq in inp {
+        if eq.can_solve(&[], &[Operator::Plus, Operator::Mul, Operator::Concat]) {
+            result += eq.target;
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "190: 10 19\n\
+                              3267: 81 40 27\n\
+                              83: 17 5\n\
+                              156: 15 6\n\
+                              7290: 6 8 6 15\n\
+                              161011: 16 10 13\n\
+                              192: 17 8 14\n\
+                              21037: 9 7 18 13\n\
+                              292: 11 6 16 20";
+
+    #[test]
+    fn test_p1() {
+        let gen = generate(TEST_INPUT);
+        let res = part1(&gen);
+        assert_eq!(res, 3749);
+    }
+
+    #[test]
+    fn test_p2() {
+        let gen = generate(TEST_INPUT);
+        let res = part2(&gen);
+        assert_eq!(res, 11387);
+    }
+}
